0eafbed0f1df328a7c2503b1ce62668e213f730c
Francesco Mineo <francymin@gmail.com>
2023-06-12 12:19:45 +0200

    add meta info

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,16 @@ const Game = () => {
     return (
         <div>
             <Head>
-                <title>MyFlappyBird</title>
+                <title>Fmineo FlappyBird React</title>
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
                 />
+                <meta name="description" content="FlappyBird game clone made with NextJS/ReactJS" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"></link>
             </Head>
             <SoundProvider>
                 <GameEngine>
